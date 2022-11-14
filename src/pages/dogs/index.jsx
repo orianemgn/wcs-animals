8ad5d98b17779a48128ae12b53f17032633d8cf4
orianemgn <mgnoriane@gmail.com>
2022-11-14 16:56:43 +0100

    step 8 done

--- src/pages/dogs/index.jsx
+++ src/pages/dogs/index.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import useFetch from "../../hooks/use-fetch";
 
 export const Dogs = () => {
-  return <div>Dogs</div>;
+  const getBreedsUrl = "https://dog.ceo/api/breeds/list/all";
+  const [breedsList, breedsListErr, breedsListLoading] = useFetch(getBreedsUrl);
+
+  if (breedsListLoading || !breedsList.message) return <div> loading </div>;
+  if (breedsListErr) return <div> error </div>;
+
+  const filteredBreeds = Object.keys(breedsList.message);
+
+  return (
+    <main>
+      {filteredBreeds.map((e, i) => (
+        <Link key={i} to={`/dogs/${e}`}>
+          {e} {""}
+        </Link>
+      ))}
+    </main>
+  );
 };
