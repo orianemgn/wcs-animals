98f9b583881323ce23d2c93bf9f83627360c244f
orianemgn <mgnoriane@gmail.com>
2022-11-14 17:36:21 +0100

    Step 9 done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Home } from "./pages/home/index";
 import { Dogs } from "./pages/dogs/index";
 import { Cats } from "./pages/cats/index";
 import { Header } from "./components/header";
+import { DogsBreedsPage } from "./pages/dogs/dogs-breed";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/cats" element={<Cats />} />
           <Route path="/dogs" element={<Dogs />} />
+          <Route path="/dogs/:id" element={<DogsBreedsPage />} />
         </Routes>
       </Router>
     </div>
