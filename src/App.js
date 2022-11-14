16729d7d566f46abb949cec87b1a924441483589
orianemgn <mgnoriane@gmail.com>
2022-11-14 16:41:01 +0100

    Step 6 done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import { Home } from "./pages/home/index";
 import { Dogs } from "./pages/dogs/index";
 import { Cats } from "./pages/cats/index";
+import { Header } from "./components/header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cats" element={<Cats />} />
