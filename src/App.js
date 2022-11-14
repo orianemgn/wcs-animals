921764312a335e8951fe0e94a429f3a2de9971e2
orianemgn <mgnoriane@gmail.com>
2022-11-14 16:31:26 +0100

    Step 1-5 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Home } from "./pages/home/index";
+import { Dogs } from "./pages/dogs/index";
+import { Cats } from "./pages/cats/index";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cats" element={<Cats />} />
+          <Route path="/dogs" element={<Dogs />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/cats/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Cats = () => {
+  return <div>Cats</div>;
+};
